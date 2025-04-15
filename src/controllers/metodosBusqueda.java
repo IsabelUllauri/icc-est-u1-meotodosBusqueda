8e5ecffd5321ef30227bdaf5c3b1071e0d18b58a
Isa U <isa.ullaurib@gmail.com>
2025-04-15 10:46:19 -0500

    primer commit nuevo repo

---
+++ src/controllers/metodosBusqueda.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import models.Persona;
+import views.ShowConsole;
+
+public class metodosBusqueda{
+
+    private int [] arreglo;
+    private ShowConsole showConsole;
+    private Persona[] people;
+
+    public metodosBusqueda(Persona[] persons){
+        this.showConsole= new ShowConsole();
+        this.people= persons;
+        showPersonByCode();
+    }
+
+    public int busquedaLineal (int[] arreglo, int valorBuscado){
+        this.arreglo= arreglo;
+        for (int i=0; i < arreglo.length; i++){
+            if (arreglo[i]==valorBuscado)
+                return i;
+            
+        }
+        return -1;
+    }
+
+    public int findPersonByCode(int code){
+        for(int i=0; i<people.length; i++){            
+        if (people[i].getCode()== code){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void showPersonByCode(){
+        int codeToFind =showConsole.getCode();
+        int indexPerson=findPersonByCode(codeToFind);
+        if(indexPerson>= 0){
+            showConsole.ShowMessage("Persona Encontrada");
+            showConsole.ShowMessage(people[indexPerson].toString());
+
+        }else{
+            showConsole.ShowMessage("persona con codigo"+codeToFind+"persona no ecnotrada");
+        }
+
+    }
+}
+
+//ayuda dios
---
+++ src/views/ShowConsole.java
@@ -0,0 +1,22 @@
+package views;
+import java.util.Scanner;
+
+public class ShowConsole {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void ShowMenu(){
+        System.out.println("Metodo Busqueda: ");
+    }
+
+    public int getCode(){
+        System.out.println("ingrese codigo: ");
+        int codigo = scanner.nextInt();
+        System.out.println("Codigo ingresado ");
+        return codigo;
+ 
+    }
+
+    public void ShowMessage(String message){
+        System.out.println(message);
+    }
+}
